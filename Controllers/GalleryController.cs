e7b648a5e0e358e6cf82f436be9ec700014a14a4
johnmcraig <jmcraig32884@gmail.com>
2017-11-04 09:56:54 -0400

    Initial Commit

---
+++ Controllers/GalleryController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleImageGallery.Data.Models;
+using SimpleImageGallery.Models;
+using System;
+using System.Collections.Generic;
+
+namespace SimpleImageGallery.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public IActionResult Index()
+        {
+            var hikingImageTags = new List<ImageTag>();
+            var cityImageTags = new List<ImageTag>();
+
+            var tag1 = new ImageTag()
+            {
+                Description = "Adventure",
+                Id = 0
+            };
+
+            var tag2 = new ImageTag()
+            {
+                Description = "Urban",
+                Id = 1
+            };
+
+            var tag3 = new ImageTag()
+            {
+                Description = "City",
+                Id = 2
+            };
+
+            hikingImageTags.Add(tag1);
+            cityImageTags.AddRange(new List<ImageTag> { tag2, tag3 });
+
+            var imageList = new List<GalleryImage>()
+            {
+                new GalleryImage()
+                {
+                    Title = "Hiking Trip",
+                    Url = "",
+                    Created = DateTime.Now,
+                    Tags = hikingImageTags
+                },
+
+                new GalleryImage()
+                {
+                    Title = "On the Trail",
+                    Url = "",
+                    Created = DateTime.Now,
+                    Tags = hikingImageTags
+                },
+
+                new GalleryImage()
+                {
+                    Title = "City",
+                    Url = "",
+                    Created = DateTime.Now,
+                    Tags = cityImageTags
+                }
+
+            };
+
+            var model = new GalleryIndexModel()
+            {
+                Images = imageList
+            };
+
+            return View();
+        }
+    }
+}
---
+++ Models/GalleryIndexModel.cs
@@ -0,0 +1,11 @@
+﻿using SimpleImageGallery.Data.Models;
+using System.Collections.Generic;
+
+namespace SimpleImageGallery.Models
+{
+    public class GalleryIndexModel
+    {
+        public IEnumerable<GalleryImage> Images { get; set; }
+        public string SearchQuery { get; set; }
+    }
+}
