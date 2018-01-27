84974904ac350c83f9199743a1f519b913d42927
johnmcraig <jmcraig32884@gmail.com>
2018-01-27 11:08:13 -0500

    Firts Build Solution and files

---
+++ Simple-Image-Gallery/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Simple_Image_Gallery.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
