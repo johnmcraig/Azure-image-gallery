d9d889fddaea43fc2b7d549d32aa488b9bc7ff56
johnmcraig <jmcraig32884@gmail.com>
2018-01-27 13:43:33 -0500

    Setup Scaffold of project

---
+++ SimpleImageGallery/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SimpleImageGallery.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
