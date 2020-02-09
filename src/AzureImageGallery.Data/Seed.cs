6de920c6f1b85e0b88cebc30e0cad0dad6811c1a
johnmcraig <jmcraig32884@gmail.com>
2020-02-08 23:33:00 -0500

    Made Seed class to insert  user names on startup

---
+++ src/AzureImageGallery.Data/Seed.cs
@@ -0,0 +1,46 @@
+﻿using AzureImageGallery.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureImageGallery.Data
+{
+    public class Seed
+    {
+        public static async Task SeedData(UserManager<AppUser> userManager)
+        {
+            if(!userManager.Users.Any())
+            {
+                var users = new List<AppUser>
+                {
+                    new AppUser
+                    {
+                        DisplayName = "Admin1",
+                        UserName = "admin1",
+                        Email = "admin1@test.com"
+                    },
+                    new AppUser
+                    {
+                        DisplayName = "Tom",
+                        UserName = "tom",
+                        Email = "tom@test.com"
+                    },
+                    new AppUser
+                    {
+                        DisplayName = "Jane",
+                        UserName = "jane",
+                        Email = "jane@test.com"
+                    }
+                };
+
+                foreach (var user in users)
+                {
+                   await userManager.CreateAsync(user, "Pa$$w0rd");
+                }
+            }
+        }
+    }
+}
