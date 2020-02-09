34afd17fa034456b995db215524fa93b333b2d4f
johnmcraig <jmcraig32884@gmail.com>
2020-02-08 23:32:16 -0500

    New migration with AppUser table

---
+++ src/AzureImageGallery.Data/Models/AppUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace AzureImageGallery.Data.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string DisplayName { get; set; }
+    }
+}
