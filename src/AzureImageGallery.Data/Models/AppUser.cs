cf2b2a36ac383b79f74ac70597bdf42a42e79f7f
johnmcraig <jmcraig32884@gmail.com>
2020-02-20 11:20:08 -0500

    Pointed AppUser to use a Guid for unique identity

--- src/AzureImageGallery.Data/Models/AppUser.cs
+++ src/AzureImageGallery.Data/Models/AppUser.cs
@@ -1,8 +1,9 @@
+using System;
 using Microsoft.AspNetCore.Identity;
 
 namespace AzureImageGallery.Data.Models
 {
-    public class AppUser : IdentityUser
+    public class AppUser : IdentityUser<Guid>
     {
         public string DisplayName { get; set; }
     }
