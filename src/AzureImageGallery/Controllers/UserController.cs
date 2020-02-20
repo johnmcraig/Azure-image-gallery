f44bb4a6f16e2bf39f88a4ebfd94b4777be681ce
johnmcraig <jmcraig32884@gmail.com>
2020-02-20 15:47:48 -0500

    injected sign in manager, created post method to register

--- src/AzureImageGallery/Controllers/UserController.cs
+++ src/AzureImageGallery/Controllers/UserController.cs
@@ -1,12 +1,51 @@
+using System.Threading.Tasks;
+using AzureImageGallery.Data.Models;
+using AzureImageGallery.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AzureImageGallery.Controllers
 {
     public class UserController : Controller
     {
+        private readonly UserManager<AppUser> _userMAnager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public UserController(UserManager<AppUser> userMAnager,
+            SignInManager<AppUser> signInManager)
+        {
+            _userMAnager = userMAnager;
+            _signInManager = signInManager;
+        }
+        
         public ActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new AppUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email
+                };
+
+                var result = await _userMAnager
+                    .CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+
+                    return RedirectToAction(nameof(HomeController.Index), "Home");
+                }
+            }
+
+            return View(model);
         }
     }
 }
