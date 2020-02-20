3c8c18d82be92bb7b466a5b096232b031829bb46
johnmcraig <jmcraig32884@gmail.com>
2020-02-20 11:19:46 -0500

    Created Register ViewModel with required properties

---
+++ src/AzureImageGallery/Models/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AzureImageGallery.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and a max of {1} characters long")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The password and confirmed password must be the same")]
+        public string ConfirmPassword { get; set; }
+    }
+}
