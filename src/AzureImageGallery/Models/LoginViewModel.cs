8295dd3d1c32c8f725fb82b1803bd1161c8b2a08
johnmcraig <jmcraig32884@gmail.com>
2020-02-20 19:30:15 -0500

    Created View Model to log in view

---
+++ src/AzureImageGallery/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AzureImageGallery.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me?")]
+        public bool RememberMe { get; set; }
+    }
+}
