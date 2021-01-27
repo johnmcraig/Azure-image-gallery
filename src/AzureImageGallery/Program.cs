17d4dac3855d0f072319326f7a7f6120633fe36b
johnmcraig <jmcraig32884@gmail.com>
2021-01-26 23:46:44 -0500

    Refactored Program class to provide services, create and migrate database on intial startup/deployment of application

--- src/AzureImageGallery/Program.cs
+++ src/AzureImageGallery/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using AzureImageGallery.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,9 +16,29 @@ namespace SimpleImageGallery
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+                try
+                {
+                    var dbContext = services.GetRequiredService<AzureImageGalleryDbContext>();
+                    await dbContext.Database.MigrateAsync();
+                    //await Seed.SeedData(dbContext);
+                }
+                catch (Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError($"Could not create or migrate database, see full error: {ex}");
+                }
+            }
+            
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
