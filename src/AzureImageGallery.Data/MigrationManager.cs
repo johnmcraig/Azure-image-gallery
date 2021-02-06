eced24a861f513e616b2b60abcceb973a92c9100
johnmcraig <jmcraig32884@gmail.com>
2021-02-05 22:05:52 -0500

    Removed pre-configured migration files. Created extension class to manage migrations and database creation. Added Hosting package.

---
+++ src/AzureImageGallery.Data/MigrationManager.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace AzureImageGallery.Data
+{
+    public static class MigrationManager
+    {
+        private static readonly ILogger _logger;
+
+        public static IHost MigratDatabase(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                using (var appContext = scope.ServiceProvider
+                    .GetRequiredService<AzureImageGalleryDbContext>())
+                {
+                    try
+                    {
+                        appContext.Database.Migrate();
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogInformation($"{ex}");
+                        throw;
+                    }
+                }
+            }
+            return host;
+        }
+    }
+}
