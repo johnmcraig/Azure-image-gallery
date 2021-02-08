dc2655e087ef02c59d5a7079f42ed5090ebb5253
johnmcraig <jmcraig32884@gmail.com>
2021-02-08 01:15:38 -0500

    Reverted back to GalleryImage

--- src/AzureImageGallery.Web/Models/GalleryIndexModel.cs
+++ src/AzureImageGallery.Web/Models/GalleryIndexModel.cs
@@ -5,7 +5,7 @@ namespace AzureImageGallery.Web.Models
 {
     public class GalleryIndexModel
     {
-        public IEnumerable<GalleryDetailModel> Images { get; set; }
+        public IEnumerable<GalleryImage> Images { get; set; }
         public string SearchQuery { get; set; }
         public int PageNumber { get; set; }
         public int PageCount { get; set; }
