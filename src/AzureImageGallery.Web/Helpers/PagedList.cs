728eba4376f4af21aea3d4d22101b73ab7294cde
johnmcraig <jmcraig32884@gmail.com>
2021-02-08 01:14:36 -0500

    Renamed to PagedList

--- src/AzureImageGallery.Web/Helpers/PagedList.cs
+++ src/AzureImageGallery.Web/Helpers/PagedList.cs
@@ -5,14 +5,14 @@ using System.Threading.Tasks;
 
 namespace AzureImageGallery.Web.Helpers
 {
-    public class PaginatedList<T> : List<T>
+    public class PagedList<T> : List<T>
     {
         public int PageSize { get; set; }
         public int TotalPages { get; set; }
         public int TotalCount { get; set; }
         public int CurrentPage { get; set; }
 
-        public PaginatedList(IEnumerable<T> items, int count, int pageNumber, int pageSize)
+        public PagedList(IEnumerable<T> items, int count, int pageNumber, int pageSize)
         {
             CurrentPage = pageNumber;
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
@@ -21,16 +21,16 @@ namespace AzureImageGallery.Web.Helpers
             AddRange(items);
         }
 
-        // Enable or Disaple paging button
+        // Enable or Disable paging button
         public bool HasPrevious => CurrentPage > 1;
         public bool HasNext => CurrentPage < TotalPages;
 
-        public static PaginatedList<T> Create(IList<T> source, int pageNumber, int pageSize)
+        public static PagedList<T> Create(IList<T> source, int pageNumber, int pageSize)
         {
             var count = source.Count();
             var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
 
-            return new PaginatedList<T>(items, count, pageNumber, pageSize);
+            return new PagedList<T>(items, count, pageNumber, pageSize);
         }
     }
 }
