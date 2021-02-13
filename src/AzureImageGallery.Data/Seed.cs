658b1519eeeb68699c7e706ac9da85ea65c8bea0
johnmcraig <jmcraig32884@gmail.com>
2021-02-12 22:42:55 -0500

    Added Id properties to objects.

--- src/AzureImageGallery.Data/Seed.cs
+++ src/AzureImageGallery.Data/Seed.cs
@@ -18,18 +18,21 @@ namespace AzureImageGallery.Data
                 {
                     new AppUser
                     {
+                        Id = Guid.NewGuid(),
                         DisplayName = "Admin1",
                         UserName = "admin1",
                         Email = "admin1@test.com"
                     },
                     new AppUser
                     {
+                        Id = Guid.NewGuid(),
                         DisplayName = "Tom",
                         UserName = "tom",
                         Email = "tom@test.com"
                     },
                     new AppUser
                     {
+                        Id = Guid.NewGuid(),
                         DisplayName = "Jane",
                         UserName = "jane",
                         Email = "jane@test.com"
@@ -48,27 +51,91 @@ namespace AzureImageGallery.Data
                 { 
                     new GalleryImage
                     {
+                        Id = 1,
                         Title = "Boat 1",
                         Created = DateTime.UtcNow,
-                        Url = "/images/boat1.jpeg"
+                        Url = "https://devimagegallery.blob.core.windows.net/images/boat1.jpeg",
+                        Tags = new List<ImageTag>
+                        {
+                           new ImageTag
+                           {
+                               Id = 1,
+                               Description = "boat",
+                               //ImageId = 1
+                           },
+                           new ImageTag
+                           {
+                               Id = 2,
+                               Description = "sea",
+                               //ImageId = 1
+                           }
+                        }
                     },
                     new GalleryImage
                     {
+                        Id = 2,
                         Title = "Boat 2",
                         Created = DateTime.UtcNow.AddDays(-30),
-                        Url = "/images/boat2.jpeg"
+                        Url = "https://devimagegallery.blob.core.windows.net/images/boat2.jpeg",
+                        Tags = new List<ImageTag>
+                        {
+                           new ImageTag
+                           {
+                               Id = 1,
+                               Description = "boat",
+                               //ImageId = 2
+                           },
+                           new ImageTag
+                           {
+                               Id = 2,
+                               Description = "sea",
+                               //ImageId = 2
+                           }
+                        }
                     },
                     new GalleryImage
                     {
+                        Id = 3,
                         Title = "Boat 3",
-                        Created = DateTime.UtcNow.AddDays(-9),
-                        Url = "https://devimagegallery.blob.core.windows.net/images/fishing-boat-denmark-beach-sea-86699.jpeg"
+                        Created = DateTime.UtcNow.AddDays(-7),
+                        Url = "https://devimagegallery.blob.core.windows.net/images/fishing-boat-denmark-beach-sea-86699.jpeg",
+                        Tags = new List<ImageTag>
+                        {
+                           new ImageTag
+                           {
+                               Id = 1,
+                               Description = "boat",
+                               //ImageId = 3
+                           },
+                           new ImageTag
+                           {
+                               Id = 2,
+                               Description = "sea",
+                               //ImageId = 3
+                           }
+                        }
                     },
                     new GalleryImage
                     {
+                        Id = 4,
                         Title = "Food",
                         Created = DateTime.UtcNow,
-                        Url = "https://devimagegallery.blob.core.windows.net/images/bread-food-healthy-breakfast.jpg"
+                        Url = "https://devimagegallery.blob.core.windows.net/images/bread-food-healthy-breakfast.jpg",
+                        Tags = new List<ImageTag>
+                        {
+                           new ImageTag
+                           {
+                               Id = 3,
+                               Description = "food",
+                               //ImageId = 4
+                           },
+                           new ImageTag
+                           {
+                               Id = 4,
+                               Description = "breakfast",
+                               //ImageId = 4
+                           }
+                        }
                     }
                 };
 
