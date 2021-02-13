81226aa715b043754afa3493c0113eb912168e03
johnmcraig <jmcraig32884@gmail.com>
2021-02-13 16:24:20 -0500

    Renamed Seed class to SeedData and only contain mock images to add to the database.

--- src/AzureImageGallery.Data/SeedData.cs
+++ src/AzureImageGallery.Data/SeedData.cs
@@ -8,72 +8,33 @@ using System.Threading.Tasks;
 
 namespace AzureImageGallery.Data
 {
-    public class Seed
+    public class SeedData
     {
-        public static void SeedData(AzureImageGalleryDbContext appContext)
+        public static void SeedImages(AzureImageGalleryDbContext appContext)
         {
-            if(!appContext.Users.Any())
-            {
-                var users = new List<AppUser>
-                {
-                    new AppUser
-                    {
-                        Id = Guid.NewGuid(),
-                        DisplayName = "Admin1",
-                        UserName = "admin1",
-                        Email = "admin1@test.com"
-                    },
-                    new AppUser
-                    {
-                        Id = Guid.NewGuid(),
-                        DisplayName = "Tom",
-                        UserName = "tom",
-                        Email = "tom@test.com"
-                    },
-                    new AppUser
-                    {
-                        Id = Guid.NewGuid(),
-                        DisplayName = "Jane",
-                        UserName = "jane",
-                        Email = "jane@test.com"
-                    }
-                };
-
-                foreach (var user in users)
-                {
-                   appContext.AddRange(user, "Pa$$w0rd");
-                }
-            }
-
-            if(!appContext.GalleryImages.Any())
+            if (!appContext.GalleryImages.Any())
             {
                 var images = new List<GalleryImage>
                 { 
                     new GalleryImage
                     {
-                        Id = 1,
                         Title = "Boat 1",
-                        Created = DateTime.UtcNow,
+                        Created = DateTime.UtcNow.AddDays(-31),
                         Url = "https://devimagegallery.blob.core.windows.net/images/boat1.jpeg",
                         Tags = new List<ImageTag>
                         {
                            new ImageTag
                            {
-                               Id = 1,
                                Description = "boat",
-                               //ImageId = 1
                            },
                            new ImageTag
                            {
-                               Id = 2,
                                Description = "sea",
-                               //ImageId = 1
                            }
                         }
                     },
                     new GalleryImage
                     {
-                        Id = 2,
                         Title = "Boat 2",
                         Created = DateTime.UtcNow.AddDays(-30),
                         Url = "https://devimagegallery.blob.core.windows.net/images/boat2.jpeg",
@@ -81,21 +42,16 @@ namespace AzureImageGallery.Data
                         {
                            new ImageTag
                            {
-                               Id = 1,
                                Description = "boat",
-                               //ImageId = 2
                            },
                            new ImageTag
                            {
-                               Id = 2,
                                Description = "sea",
-                               //ImageId = 2
                            }
                         }
                     },
                     new GalleryImage
                     {
-                        Id = 3,
                         Title = "Boat 3",
                         Created = DateTime.UtcNow.AddDays(-7),
                         Url = "https://devimagegallery.blob.core.windows.net/images/fishing-boat-denmark-beach-sea-86699.jpeg",
@@ -103,21 +59,16 @@ namespace AzureImageGallery.Data
                         {
                            new ImageTag
                            {
-                               Id = 1,
                                Description = "boat",
-                               //ImageId = 3
                            },
                            new ImageTag
                            {
-                               Id = 2,
                                Description = "sea",
-                               //ImageId = 3
                            }
                         }
                     },
                     new GalleryImage
                     {
-                        Id = 4,
                         Title = "Food",
                         Created = DateTime.UtcNow,
                         Url = "https://devimagegallery.blob.core.windows.net/images/bread-food-healthy-breakfast.jpg",
@@ -125,15 +76,11 @@ namespace AzureImageGallery.Data
                         {
                            new ImageTag
                            {
-                               Id = 3,
                                Description = "food",
-                               //ImageId = 4
                            },
                            new ImageTag
                            {
-                               Id = 4,
                                Description = "breakfast",
-                               //ImageId = 4
                            }
                         }
                     }
