dfa95459f98e3b413d22ae05241d0ee70e7ee6fe
johnmcraig <jmcraig32884@gmail.com>
2022-06-07 23:48:09 -0400

    Added two more extension methods to search by title and filter by tags

--- src/AzureImageGallery.Web/Extensions/GalleryImageExtensions.cs
+++ src/AzureImageGallery.Web/Extensions/GalleryImageExtensions.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using AzureImageGallery.Data.Models;
 
@@ -15,6 +16,27 @@ namespace AzureImageGallery.Web.Extensions
                 "dateDecs" => query.OrderByDescending(i => i.Created),
                 _ => query.OrderBy(i => i.Title)
             };
+
+            return query;
+        }
+
+        public static IQueryable<GalleryImage> Search(this IQueryable<GalleryImage> query, string searchTerm)
+        {
+            if(string.IsNullOrEmpty(searchTerm)) return query;
+
+            var lowerCaseSearchTerm = searchTerm.Trim().ToLower();
+            
+            return query.Where(i => i.Title.ToLower().Contains(searchTerm));
+        }
+
+        public static IQueryable<GalleryImage> Filter(this IQueryable<GalleryImage> query, string tags)
+        {
+            var imageList = new List<string>();
+
+            if(!string.IsNullOrEmpty(tags))
+                imageList.AddRange(tags.ToLower().Split(", ").ToList());
+
+            query = query.Where(i => imageList.Count == 0 || imageList.Contains(i.Tags.ToString().ToLower()));
 
             return query;
         }
